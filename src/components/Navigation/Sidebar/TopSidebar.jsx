168ed44eee55aaea4f32b2f023a30e7004554da4
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-04-04 08:06:06 +0700

    Add TopSidebar component for the Sidebar and implement search functionality with shortcut key and search bar

---
+++ src/components/Navigation/Sidebar/TopSidebar.jsx
@@ -0,0 +1,115 @@
+import { CubeTransparentIcon, SearchIcon } from 'components/Icon';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+
+import { IconButton } from 'components/Action';
+import { IconWrapper } from 'components/DataDisplay';
+import Layer from 'components/Layout/Layer';
+import { Popover } from 'components/OverLay';
+import SearchMain from 'features/search/components/SearchMain';
+import clsx from 'clsx';
+import { routePaths } from 'routes/routeConfig';
+import { setReload } from 'store/slices/appSlice';
+
+export function TopSidebar() {
+	const { pathname } = useLocation();
+	const isReload = useSelector((state) => state.app.isReload);
+	const dispatch = useDispatch();
+	const handleReload = (e) => {
+		if (pathname !== routePaths.HOME && !isReload) return;
+		e.preventDefault();
+		dispatch(setReload(true));
+	};
+
+	return (
+		<Layer
+			level={0}
+			className="mx-1 mb-2 flex w-14 flex-col items-center justify-center gap-2 rounded-lg rounded-b-lg  py-2"
+		>
+			<Link
+				to={routePaths.HOME}
+				className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-b from-primary-400 to-primary-900 font-bold text-slate-100"
+				onClick={handleReload}
+			>
+				<IconWrapper>
+					<CubeTransparentIcon strokeWidth={3} />
+				</IconWrapper>
+			</Link>
+			<Search />
+		</Layer>
+	);
+}
+
+function Search() {
+	const popoverRef = useRef(null);
+	const searchRef = useRef(null);
+	const [isFocused, setIsFocused] = useState(false);
+	useEffect(() => {
+		const handleKeyDown = (e) => {
+			if ((e.ctrlKey && e.key === 'k') || (e.ctrlKey && e.key === 'K')) {
+				e.preventDefault();
+				popoverRef.current.toggle();
+			}
+		};
+		window.addEventListener('keydown', handleKeyDown);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+	return (
+		<Popover
+			ref={popoverRef}
+			placement={'right-start'}
+			hideOnClickOutside={!isFocused}
+			onHide={() => {
+				setIsFocused(false);
+				searchRef?.current.blur();
+			}}
+			offset={[-54, 28]}
+			onShow={() => {
+				setIsFocused(true);
+				if (searchRef?.current) searchRef?.current.focus();
+			}}
+			render={
+				<Popover.Content
+					level={1}
+					className={clsx(
+						'h-[104px] w-80 !rounded-lg p-2 shadow-3xl transition-all ',
+						isFocused ? 'px-0 pt-0' : '',
+					)}
+				>
+					<Popover.Arrow />
+					<SearchMain
+						onFocus={() => {
+							setIsFocused(true);
+						}}
+						onBlur={() => {
+							setIsFocused(false);
+						}}
+						ref={searchRef}
+						placeholder="Search for Nov"
+					/>
+				</Popover.Content>
+			}
+		>
+			{({ visible }) => (
+				<IconButton
+					color={'secondary'}
+					rounded
+					variant={visible ? 'filled' : 'text'}
+				>
+					<IconWrapper>
+						<SearchIcon
+							className={
+								visible
+									? 'text-primary-700 dark:text-primary-500'
+									: ''
+							}
+						/>
+					</IconWrapper>
+				</IconButton>
+			)}
+		</Popover>
+	);
+}
