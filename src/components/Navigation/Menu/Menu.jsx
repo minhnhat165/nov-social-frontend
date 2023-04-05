317ffb2dc958a8c65a2468444c9af5ad249ef3fb
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-04-05 20:54:46 +0700

    Add new BookMarkSlashIcon and Menu components with related functionalities

--- src/components/Navigation/Menu/Menu.jsx
+++ src/components/Navigation/Menu/Menu.jsx
@@ -1,14 +1,35 @@
-import { IconWrapper } from 'components/DataDisplay';
-import Layer from 'components/Layout/Layer';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Text } from 'components/Typography';
+import React, { createContext, useContext, useState } from 'react';
 
+import { EllipsisHorizontalIcon } from 'components/Icon';
+import { IconButton } from 'components/Action';
+import { IconWrapper } from 'components/DataDisplay';
+import { Popover } from 'components/OverLay';
+import PropTypes from 'prop-types';
+import { Text } from 'components/Typography';
+import clsx from 'clsx';
+
+const MenuContext = createContext({
+	popoverRef: null,
+	setPopoverRef: () => {},
+	triggerRef: null,
+	setTriggerRef: () => {},
+});
+
+const useMenu = () => useContext(MenuContext);
 const Menu = ({ children }) => {
+	const [popoverRef, setPopoverRef] = useState(null);
+	const [triggerRef, setTriggerRef] = useState(null);
 	return (
-		<Layer level={0} className="p-2">
+		<MenuContext.Provider
+			value={{
+				popoverRef,
+				setPopoverRef,
+				triggerRef,
+				setTriggerRef,
+			}}
+		>
 			{children}
-		</Layer>
+		</MenuContext.Provider>
 	);
 };
 
@@ -16,7 +37,7 @@ const Item = ({ icon, children, ...props }) => {
 	return (
 		<div
 			{...props}
-			className="flex h-10 cursor-pointer gap-2 rounded-lg p-2 hover:bg-slate-200 dark:hover:bg-dark-800"
+			className="flex h-10 cursor-pointer items-center gap-2 rounded-lg p-2 hover:bg-slate-200 dark:hover:bg-dark-800"
 		>
 			{icon && (
 				<Text>
@@ -29,6 +50,54 @@ const Item = ({ icon, children, ...props }) => {
 };
 
 Menu.Item = Item;
+
+const Trigger = ({ children, ...props }) => {
+	const { popoverRef, setTriggerRef } = useMenu();
+	return (
+		<div ref={setTriggerRef} className="ml-auto">
+			<IconButton
+				onClick={() => {
+					popoverRef?.toggle();
+				}}
+				rounded
+				color={'secondary'}
+				variant="text"
+				{...props}
+			>
+				{children ? children : <EllipsisHorizontalIcon />}
+			</IconButton>
+		</div>
+	);
+};
+
+Menu.Trigger = Trigger;
+
+const Content = ({ children, className, ...props }) => {
+	const { setPopoverRef, triggerRef } = useMenu();
+	return (
+		<Popover
+			ref={setPopoverRef}
+			reference={triggerRef}
+			placement="bottom-end"
+			hideOnClick
+			offset={[5, 5]}
+			render={
+				<Popover.Content
+					level={0}
+					className={clsx(
+						'flex flex-col gap-2 drop-shadow-[0_0_6px_rgba(0,0,0,0.2)] dark:shadow-2xl',
+						className,
+					)}
+				>
+					<Popover.Arrow />
+					{children}
+				</Popover.Content>
+			}
+		/>
+	);
+};
+
+Menu.Content = Content;
 
 Menu.propTypes = {
 	children: PropTypes.node,
