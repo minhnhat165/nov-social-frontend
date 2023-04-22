40eadc88f0df7d218dd072625fcc86998960a1f0
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-04-22 17:09:23 +0700

    Add LazyTippySingleton component and update Lab page layout

--- src/pages/Lab/Lab.jsx
+++ src/pages/Lab/Lab.jsx
@@ -2,7 +2,13 @@ import './lab.css';
 
 const Components = () => {
 	return (
-		<div className="flex h-screen w-screen flex-col items-center justify-center"></div>
+		<div className="relative flex h-screen w-screen flex-col items-center justify-center">
+			<div className="relative">
+				<div className="absolute left-0 top-0 z-10 h-10 w-10 bg-blue-500"></div>
+				<div className="relative h-10 w-10 bg-white"></div>
+				<div className="h-10 w-10 bg-pink-400"></div>
+			</div>
+		</div>
 	);
 };
 
