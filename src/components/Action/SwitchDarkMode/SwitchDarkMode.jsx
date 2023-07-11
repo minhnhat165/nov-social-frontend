674f9b81e7171cc627c6a25189deed94227fe61b
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-07-11 21:38:32 +0700

    fix comment post detail

--- src/components/Action/SwitchDarkMode/SwitchDarkMode.jsx
+++ src/components/Action/SwitchDarkMode/SwitchDarkMode.jsx
@@ -3,5 +3,5 @@ import useDarkMode from '../../../hooks/useDarkMode';
 
 export const SwitchDarkMode = () => {
 	const [isDarkMode, toggleDarkMode] = useDarkMode();
-	return <SwitchButton checked={isDarkMode} onChange={toggleDarkMode} />;
+	return <SwitchButton isOn={isDarkMode} onChange={toggleDarkMode} />;
 };
