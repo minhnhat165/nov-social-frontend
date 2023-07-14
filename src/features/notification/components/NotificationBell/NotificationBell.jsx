8dfdbdfe9c9dc2d493cd8e583d83cacbf80667bd
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-07-14 20:54:04 +0700

    feed: add notify sound

--- src/features/notification/components/NotificationBell/NotificationBell.jsx
+++ src/features/notification/components/NotificationBell/NotificationBell.jsx
@@ -1,11 +1,13 @@
-import { readNotify } from 'api/userApi';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
 import { Badge } from 'components/DataDisplay';
 import { BellIcon } from 'components/Icon';
+import notifySound from 'assets/sounds/notify.wav';
+import { readNotify } from 'api/userApi';
 import socket from 'configs/socket-config';
-import { useEffect, useState } from 'react';
-import { useMutation } from 'react-query';
-import { useDispatch, useSelector } from 'react-redux';
 import { updateUser } from 'store/slices/authSlice';
+import { useMutation } from 'react-query';
 
 export const NotificationBell = () => {
 	const dispatch = useDispatch();
@@ -23,6 +25,7 @@ export const NotificationBell = () => {
 	useEffect(() => {
 		socket.on('server.notification.count', (num) => {
 			setNumNotifications(num);
+			new Audio(notifySound).play();
 		});
 		return () => {
 			socket.off('server.notification.count');
